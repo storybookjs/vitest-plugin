1ec31579183f0f3f3302d094903fd4cec7fc3ff8
Yann Braga <yannbf@gmail.com>
2024-07-31 11:41:54 +0200

    wip

--- packages/vitest-plugin-storybook/tsup.config.ts
+++ packages/vitest-plugin-storybook/tsup.config.ts
@@ -4,12 +4,10 @@ export default defineConfig([
   {
     entry: ['src/index.ts', 'src/test-utils.ts', 'src/global-setup.ts'],
     format: ['esm', 'cjs'],
-    clean: true,
     dts: true,
   },
   {
     entry: ['src/setup-file.ts'],
     format: 'esm',
-    clean: true,
   },
 ])
